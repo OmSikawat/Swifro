161dacd847e3ef4211f3b30259068ee7bf3a4048
OmSikawat <omsikawat67@gmail.com>
2025-07-19 15:44:12 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,10 +6,10 @@ export default function Card(props) {
     <div className='w-[280px] shrink-0 mb-3'>
       <div className='group h-[182px] rounded-[15px] overflow-hidden relative'>
         <img
-          className='group-hover:scale-110 duration-150 object-cover w-full h-full'
-          src={`http://localhost:5000/images/${props.image}`}
-          alt={props.title}
-        />
+  className='group-hover:scale-110 duration-150 object-cover w-full h-full'
+  src={`https://swiapi.vercel.app/images/${props.image}`}  // localhost se change kiya
+  alt={props.title}
+/>
         <div className='image-overlay absolute w-full h-full top-0 flex items-end p-2 text-[20px] font-bold text-white tracking-tighter'>
           {props.offer}
         </div>
